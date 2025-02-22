78a7d9d63783335609ce791bb0e30952ab96027d
Daeman-dev <69400081+Daeman-dev@users.noreply.github.com>
2025-02-22 16:43:28 +0300

    Изменение времени дейлика на 9:00

--- src/features/regular/regular.py
+++ src/features/regular/regular.py
@@ -8,7 +8,7 @@ from src.features.daily.regular import send_mentions, send_weekly_statistics
 
 # Задача для выполнения каждый день в определённое время
 def schedule_daily_tasks(bot):
-    schedule.every().day.at("06:00").do(lambda: asyncio.create_task(send_mentions(bot)))
+    schedule.every().day.at("09:00").do(lambda: asyncio.create_task(send_mentions(bot)))
     schedule.every().day.at("00:00").do(lambda: asyncio.create_task(check_birthdays(bot)))  # Проверка в полночь
     schedule.every().day.at("00:00").do(lambda: asyncio.create_task(remove_birthday_roles(bot)))  # Проверка в полночь
     schedule.every().sunday.at("12:00").do(
